73e960cc6a07c054ea90c84dac3a45464d8990c8
Nefelijm <sistemasnik20@gmail.com>
2018-01-26 02:11:19 -0500

    Creando variables y funcion para decifrar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-  alert('Estoy aqui');
+  // Declarando Variables
+
+  let input = $('#encryption');  
+  let btnDecipher = $('#decipher');
+  let btnCipher = $('#cipher');
+  let content = $('#conten');
+  let centinel = false;
+	
+  // Funcion para el boton Cifrar
+
+  btnCipher.on('click', () => {
+    let valueInput = input.val();
+    if (valueInput.match(/[a-z]/i)) {
+      let cifrado = '';
+      for (let i = 0; i < valueInput.length; i++) {
+        let toAscci = valueInput.charCodeAt(i);
+        let toAscci2 = valueInput[i];
+        if ((toAscci >= 65) && (toAscci <= 90)) {
+          toAscci2 = String.fromCharCode(((toAscci - 65 + 33) % 26) + 65);
+        } else if ((toAscci >= 97) && (toAscci <= 122)) {
+          toAscci2 = String.fromCharCode(((toAscci - 97 + 33) % 26) + 97);
+        }
+        cifrado += toAscci2;
+      } 
+      content.text(cifrado);
+    } else {
+      alert('Ingresa un Texto Válido');
+    }
+  });
 });
